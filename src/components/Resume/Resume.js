01a32fdc7da593e4b3ec745ffca8080149b0604b
Ian Holmes <concord511@gmail.com>
2022-01-17 12:39:21 -0600

    fixed Resume page font sizes for smaller screens

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -5,7 +5,7 @@ import resume from '../../assets/Ani-Holmes-Resume.pdf';
 function Resume() {
     return (
         <div className="resume__wrapper">
-            <h2>Resume:<a className="resume__downloadLink" href="../../assets/images/Ani-Holmes-Resume.pdf" download="Download Resume">(Download)</a></h2>
+            <span className="resume__title">Resume:<a className="resume__downloadLink" href="../../assets/images/Ani-Holmes-Resume.pdf" download="Download Resume">(Download)</a></span>
             <iframe title="Resume Viewer" src={resume}></iframe>
         </div>
     )
